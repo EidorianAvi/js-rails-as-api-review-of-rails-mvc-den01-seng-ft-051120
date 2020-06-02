bc6ab17f4fb4072e60602370b6d7e842cb75e79e
Rene Avila <reneavila1993@gmail.com>
2020-06-02 02:45:52 +0000

    Automatically backed up by Learn

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 # Add code from Readme
 
 class Bird < ApplicationRecord
 
 end
+=======
+>>>>>>> 8309bab525c3ab7ce86d420077c35eb85058672e
--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,4 +1,5 @@
 # Add code from Readme
+<<<<<<< HEAD
 
 class CreateBirds < ActiveRecord::Migration[5.2]
     def change
@@ -10,3 +11,15 @@ class CreateBirds < ActiveRecord::Migration[5.2]
       end
     end
   end
+=======
+class CreateBirds < ActiveRecord::Migration[5.2]
+  def change
+    create_table :birds do |t|
+      t.string :name
+      t.string :species
+ 
+      t.timestamps
+    end
+  end
+end
+>>>>>>> 8309bab525c3ab7ce86d420077c35eb85058672e
